d927c25b9913590066d9059502f2b54df21f45ab
Oscar Estrada <oscar.estrada@inide.gob.ni>
2021-04-10 16:41:45 -0600

    programacion de servicios y asignaciones

--- src/app/routes/fleet/fleet.module.ts
+++ src/app/routes/fleet/fleet.module.ts
@@ -3,8 +3,10 @@ import { SharedModule } from '@shared/shared.module';
 import { FleetRoutingModule } from './fleet-routing.module';
 import { FleetDriversComponent } from './drivers/drivers.component';
 import { FleetVehiclesComponent } from './vehicles/vehicles.component';
+import { FleetServiceschedulesComponent } from './serviceschedules/serviceschedules.component';
+import { FleetAssignmentsComponent } from './assignments/assignments.component';
 
-const COMPONENTS = [FleetDriversComponent, FleetVehiclesComponent];
+const COMPONENTS = [FleetDriversComponent, FleetVehiclesComponent, FleetServiceschedulesComponent, FleetAssignmentsComponent];
 const COMPONENTS_DYNAMIC = [];
 
 @NgModule({
