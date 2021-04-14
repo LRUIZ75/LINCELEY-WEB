bdb727e3631bf67b3c5357672ea1d3983dfe4616
Oscar Estrada <oscar.estrada@inide.gob.ni>
2021-04-14 12:30:36 -0600

    probando vehicle

--- src/app/routes/fleet/vehicles/addvehicle/addvehicle.component.ts
+++ src/app/routes/fleet/vehicles/addvehicle/addvehicle.component.ts
@@ -1,169 +1,246 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatCheckboxChange } from '@angular/material/checkbox';
+//import { MatCheckboxChange } from '@angular/material/checkbox';
 import {
   Vehicle,
   VehiclesService,
   Company,
-  CompaniesService
+  CompaniesService,
+  Person,
+  PeopleService,
 } from 'app/services';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-addvehicle',
   templateUrl: './addvehicle.component.html',
-  styleUrls: ['./addvehicle.component.scss']
+  styleUrls: ['./addvehicle.component.scss'],
 })
 export class AddvehicleComponent implements OnInit {
-/**
+  /**
    * Formulario de Agregar/Editar
    */
- vehicleFormGroup: FormGroup;
- vehicle: Vehicle;
+  vehicleFormGroup: FormGroup;
+  vehicle: Vehicle;
 
- public companyList: any[] = [];
- public companies: any[] = [];
+  public typeList: [{"id":"AUTO","type":"AUTO"}, {"id":"MOTO","type":"MOTO"}, {"id":"PICKUP","type":"PICKUP"}, {"id":"TRUCK","type":"TRUCK"}];
+  public companyList: any[] = [];
+  public companies: any[] = [];
+  public personList: any[] = [];
+  public people: any[] = [];
+  public filesRegistrationCard: File[] = [];
+  public filesInsuranceCard: File[] = [];
 
- @Output() changeStateEvent = new EventEmitter<string>();
+  @Output() changeStateEvent = new EventEmitter<string>();
 
- @Input() formMode = 'ADD';
- @Input() initialData: any = {};
+  @Input() formMode = 'ADD';
+  @Input() initialData: any = {};
 
- constructor(
-   private formBuilder: FormBuilder,
-   private companyService: CompaniesService,
-   private vehicleService: VehiclesService,
-   private toaster: ToastrService
- ) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private companyService: CompaniesService,
+    private personService: PeopleService,
+    private vehicleService: VehiclesService,
+    private toaster: ToastrService
+  ) {}
 
- ngOnInit(): void {
-   this.vehicleFormGroup = this.formBuilder.group({
-     plateNumber:['', [Validators.required]],
-     vehicleType:['', [Validators.required]],
-     brand:['', [Validators.required]],
-     model: ['', [Validators.required]],
-     year: '',
-     color: '',
-     company: '',
-     isExternal: [false, [Validators.required]],
-     isActive: [true, [Validators.required]],
-     isAvailable: [true, [Validators.required]],
-     registrationCard: '',
-     insuranceCard: '',
-     owner: '',
-   });
+  ngOnInit(): void {
+    this.vehicleFormGroup = this.formBuilder.group({
+      plateNumber: ['', [Validators.required]],
+      vehicleType: ['', [Validators.required]],
+      brand: ['', [Validators.required]],
+      model: ['', [Validators.required]],
+      year: [''],
+      color: [''],
+      company: [''],
+      isExternal: [false, [Validators.required]],
+      isActive: [true, [Validators.required]],
+      isAvailable: [true, [Validators.required]],
+      registrationCard: [''],
+      insuranceCard: [''],
+      owner: ['']
+    });
 
-   if (this.formMode == 'EDIT' && this.initialData) {
-     this.vehicleFormGroup.patchValue(this.initialData as Vehicle);
-   }
+    if (this.formMode == 'EDIT' && this.initialData) {
+      this.vehicleFormGroup.patchValue(this.initialData as Vehicle);
+    }
 
-   // obtener listas requeridas de las que depende el componente
-   //build Company List for Selecte
-   this.getCompanyList();
- }
+    //deshabilitar fotos
+    this.vehicleFormGroup.get('registrationCard').disable();
+    this.vehicleFormGroup.get('insuranceCard').disable();
 
- getCompanyList() {
-   this.companyService
-     .getData()
-     .toPromise()
-     .then(response => {
-       if (!response) {
-         this.toaster.error('No hay datos de compañías!');
-         return;
-       }
-       this.companies = response.objects;
-       switch (this.formMode) {
-         case 'ADD':
-           this.companyList = this.companies.filter(company => company.isActive == true);
-           break;
-         default:
-           this.companyList = this.companies;
-       }
-     })
-     .catch(err => {
-       this.toaster.error(err.message);
-     });
- }
+    // obtener listas requeridas de las que depende el componente
+    //build Company List for Selecte
+    this.getCompanyList();
+    this.getPersonList();
+  }
 
+  getCompanyList() {
+    this.companyService
+      .getData()
+      .toPromise()
+      .then(response => {
+        if (!response) {
+          this.toaster.error('No hay datos de compañías!');
+          return;
+        }
+        this.companies = response.objects;
+        switch (this.formMode) {
+          case 'ADD':
+            this.companyList = this.companies.filter(company => company.isActive == true);
+            break;
+          default:
+            this.companyList = this.companies;
+        }
+      })
+      .catch(err => {
+        this.toaster.error(err.message);
+      });
+  }
+  getPersonList(isExternal: boolean = false) {
+    this.personService
+      .getData()
+      .toPromise()
+      .then(response => {
+        if (!response) {
+          this.toaster.error('No hay datos de personas!');
+          return;
+        }
+        this.people = response.objects;
 
-// PROPIEDADES
- get company() {
-   return this.vehicleFormGroup.get('company');
- }
+        this.personList = this.people;
+      })
+      .catch(err => {
+        this.toaster.error(err.message);
+      });
+  }
 
+  updatePicture(fieldName: string, id: string) {
+    //update picture data
+    var files: File[] = [];
+    if (fieldName == 'registrationCard') files = this.filesRegistrationCard;
 
- /**
-  * Resetea el valor de todos los campos
-  */
- onReset() {
-   this.vehicleFormGroup.reset();
- }
+    if (fieldName == 'insuranceCard') files = this.filesInsuranceCard;
 
- /**
-  * Cancela el modo de agregar/editar
-  */
- onCancel() {
-   this.changeState('RETRIEVE');
- }
+    this.vehicleService
+      .updatePicture(fieldName, id, files[0])
+      .toPromise()
+      .then(resp => {
+        if (!resp) {
+          this.toaster.error('Operación fallida!');
+          return;
+        }
+      })
+      .catch(err => {
+        this.toaster.error(err);
+      });
+  }
 
- /**
-  * Cambia el estado actual de acceso a datos
-  * @param value Nuevo Estado
-  */
- changeState(value: string) {
-   this.changeStateEvent.emit(value);
- }
+  // PROPIEDADES
+  get company() {
+    return this.vehicleFormGroup.get('company');
+  }
+  get owner() {
+    return this.vehicleFormGroup.get('owner');
+  }
 
- onSubmit() {
-   if (!this.vehicleFormGroup.valid) {
-     this.toaster.warning('El formulario tiene erorres!');
-     return;
-   }
+  /**
+   * Resetea el valor de todos los campos
+   */
+  onReset() {
+    this.vehicleFormGroup.reset();
+  }
 
-   this.vehicle = <Vehicle>this.vehicleFormGroup.value;
+  /**
+   * Cancela el modo de agregar/editar
+   */
+  onCancel() {
+    this.changeState('RETRIEVE');
+  }
 
-   switch (this.formMode) {
-     case 'EDIT':
-       this.vehicleService
-         .updateData(this.initialData._id, this.vehicle)
-         .toPromise()
-         .then(resp => {
-           if (!resp) {
-             this.toaster.error('Operación fallida!');
-             return;
-           }
+  /**
+   * Cambia el estado actual de acceso a datos
+   * @param value Nuevo Estado
+   */
+  changeState(value: string) {
+    this.changeStateEvent.emit(value);
+  }
 
-           this.vehicle = <Vehicle>resp.updated;
-           this.toaster.success('Operación exitosa!');
-           this.changeState('RETRIEVE');
-         })
-         .catch(err => {
-           this.toaster.error(err);
-         });
-       break;
+  onSubmit() {
+    if (!this.vehicleFormGroup.valid) {
+      this.toaster.warning('El formulario tiene erorres!');
+      return;
+    }
 
-     case 'ADD':
-       this.vehicleService
-         .addData(this.vehicle)
-         .toPromise()
-         .then(resp => {
-           if (!resp) {
-             this.toaster.error('Operación fallida!');
-             return;
-           }
+    this.vehicle = <Vehicle>this.vehicleFormGroup.value;
 
-           this.vehicle = <Vehicle>resp.created;
-           this.toaster.success('Operación exitosa!');
-           this.changeState('RETRIEVE');
-         })
-         .catch(err => {
-           this.toaster.error(err.message);
-         });
-       break;
+    switch (this.formMode) {
+      case 'EDIT':
+        this.vehicleService
+          .updateData(this.initialData._id, this.vehicle)
+          .toPromise()
+          .then(resp => {
+            if (!resp) {
+              this.toaster.error('Operación fallida!');
+              return;
+            }
 
-     default:
-       this.toaster.warning('Se desconoce el modo del formulario');
-   }
- }
- 
+            this.vehicle = <Vehicle>resp.updated;
+
+            if (this.filesRegistrationCard.length > 0)
+              //nueva foto de registro
+              this.updatePicture('registrationCard', this.initialData._id);
+
+            if (this.filesInsuranceCard.length > 0)
+              //nueva foto de registro
+              this.updatePicture('insuranceCard', this.initialData._id);
+
+            this.toaster.success('Operación exitosa!');
+            this.changeState('RETRIEVE');
+          })
+          .catch(err => {
+            this.toaster.error(err);
+          });
+        break;
+
+      case 'ADD':
+        this.vehicleService
+          .addData(this.vehicle)
+          .toPromise()
+          .then(resp => {
+            if (!resp) {
+              this.toaster.error('Operación fallida!');
+              return;
+            }
+
+            this.vehicle = <Vehicle>resp.created;
+            this.toaster.success('Operación exitosa!');
+            this.changeState('RETRIEVE');
+          })
+          .catch(err => {
+            this.toaster.error(err.message);
+          });
+        break;
+
+      default:
+        this.toaster.warning('Se desconoce el modo del formulario');
+    }
+  }
+
+  onSelectRegistracionCard(event) {
+    console.log(event);
+    if (this.filesRegistrationCard.length > 0) {
+      this.filesRegistrationCard = [];
+    }
+    this.filesRegistrationCard.push(...event.addedFiles);
+    this.vehicleFormGroup.get('picture').setValue(this.filesRegistrationCard[0].name);
+  }
+  onSelectInsuranceCard(event) {
+    console.log(event);
+    if (this.filesInsuranceCard.length > 0) {
+      this.filesInsuranceCard = [];
+    }
+    this.filesInsuranceCard.push(...event.addedFiles);
+    this.vehicleFormGroup.get('picture').setValue(this.filesInsuranceCard[0].name);
+  }
 }
