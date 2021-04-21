75679d69c423038974d2c6293dbbd068b45d0d94
Luis Danilo Ruiz Tórrez <lruiz75@gmail.com>
2021-04-21 03:18:09 -0600

    Corecciones y mejoras UI que deben servir como plantillas de rutas (base = Companies)

--- src/app/routes/fleet/vehicles/vehicles.component.ts
+++ src/app/routes/fleet/vehicles/vehicles.component.ts
@@ -6,84 +6,59 @@ import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDialogComponent } from '@shared/components/confirm-dialog/confirm-dialog.component';
 
 //Import services
-import { Company, CompaniesService, Vehicle, VehiclesService, Person, PeopleService} from 'app/services';
+import {
+  Company,
+  CompaniesService,
+  Vehicle,
+  VehiclesService,
+  Person,
+  PeopleService,
+} from 'app/services';
 import { DataTableTranslations } from 'ornamentum';
 
 @Component({
   selector: 'app-fleet-vehicles',
   templateUrl: './vehicles.component.html',
   styleUrls: ['./vehicles.component.scss'],
-  providers: [CompaniesService, VehiclesService]
+  providers: [CompaniesService, VehiclesService],
 })
 export class FleetVehiclesComponent implements OnInit {
-
   /* Variables locales */
 
   public currentState: string = 'RETRIEVE';
-  public selected:  Vehicle;
+  public selected: Vehicle;
 
-  public vehicleList: any[]=[];
-  public companyList: any[] = [];
-  public personList: any[]=[];
+  public vehicleList: any[] = [];
+
   public title: string;
   dragging = false;
   opened = false;
 
-  public dataTableTranslations: DataTableTranslations = {
-    pagination: {
-      limit: this.translate.instant('pagination.limit'),
-      rangeKey: this.translate.instant('pagination.records'),
-      rangeSeparator: this.translate.instant('pagination.of'),
-      nextTooltip: this.translate.instant('pagination.next'),
-      previousTooltip: this.translate.instant('pagination.previous'),
-      lastTooltip: this.translate.instant('pagination.last'),
-      firstTooltip: this.translate.instant('pagination.first'),
-    },
-  };
+  public dataTableTranslations: DataTableTranslations;
 
   getDataTableTranslations(): DataTableTranslations {
     this.dataTableTranslations = {
-     pagination: {
-       limit: this.translate.instant('pagination.limit'),
-       rangeKey: this.translate.instant('pagination.records'),
-       rangeSeparator: this.translate.instant('pagination.of'),
-       nextTooltip: this.translate.instant('pagination.next'),
-       previousTooltip: this.translate.instant('pagination.previous'),
-       lastTooltip: this.translate.instant('pagination.last'),
-       firstTooltip: this.translate.instant('pagination.first'),
-     },
-     noDataMessage: this.translate.instant('notifications.nodata'),
-     dropdownFilter: {
-       filterPlaceholder: this.translate.instant('record_actions.search'),
-       selectPlaceholder: this.translate.instant('record_actions.search')
-     },
-     columnSelector: { header: ">>"}
-
-
-   };
-   return this.dataTableTranslations;
- }
-
-  getCompanyList() {
-    this.companyService
-      .getData()
-      .toPromise()
-      .then(resp => {
-        this.companyList = resp.objects;
-      });
-  }
-
-  getPersonList() {
-    this.personService
-      .getData()
-      .toPromise()
-      .then(resp => {
-        this.personList = resp.objects;
-      });
+      pagination: {
+        limit: this.translate.instant('pagination.limit'),
+        rangeKey: this.translate.instant('pagination.records'),
+        rangeSeparator: this.translate.instant('pagination.of'),
+        nextTooltip: this.translate.instant('pagination.next'),
+        previousTooltip: this.translate.instant('pagination.previous'),
+        lastTooltip: this.translate.instant('pagination.last'),
+        firstTooltip: this.translate.instant('pagination.first'),
+      },
+      noDataMessage: this.translate.instant('notifications.nodata'),
+      dropdownFilter: {
+        filterPlaceholder: this.translate.instant('record_actions.search'),
+        selectPlaceholder: this.translate.instant('record_actions.search'),
+      },
+      columnSelector: { header: '>>' },
+    };
+    return this.dataTableTranslations;
   }
 
 
-  @Input() filter: string = '';
+  @Input() companyFilter: string = '';
 
   constructor(
     public companyService: CompaniesService,
@@ -94,58 +69,53 @@ export class FleetVehiclesComponent implements OnInit {
     private confirmDialog: MatDialog
   ) {
     this.title = this.translate.instant('domain.vehicles');
-    this.getCompanyList();
-    this.getPersonList();
+
     this.getList();
   }
 
-  getTitle()
-  {
+  getTitle() {
     this.title = this.translate.instant('domain.vehicles');
     return this.title;
   }
 
   ngOnInit() {
-
-    if('geolocation' in navigator) {
+    if ('geolocation' in navigator) {
       console.log('geolocation is available');
     } else {
       console.log('geolocation is NOT available');
     }
-
   }
 
   getList() {
-
     this.vehicleService.getData().subscribe(
       res => {
         if (res) {
-            var jsonResponse = JSON.stringify(res);
-            var response = JSON.parse(jsonResponse);
-            if (response.status != 'ok') return;
-            this.vehicleList = response.objects;
-            this.vehicleList = this.vehicleList.filter(it => it.isActive == true);
-            if (this.filter) {
-              this.vehicleList = this.vehicleList.filter(
-                it => it.isActive == true && it.company == this.filter
-              );
-            }
+          var jsonResponse = JSON.stringify(res);
+          var response = JSON.parse(jsonResponse);
+          if (response.status != 'ok') return;
+          this.vehicleList = response.objects;
+          this.vehicleList = this.vehicleList.filter(it => it.isActive == true);
+          if (this.companyFilter) {
+            this.vehicleList = this.vehicleList.filter(
+              it => it.isActive == true && it.company._id == this.companyFilter
+            );
+          }
 
-            for (var i = 0; i < this.vehicleList.length; i++) {
-              var comp = this.companyList.find(it => it._id == this.vehicleList[i].company);
-              this.vehicleList[i].companyName = !comp?"":comp.fullName;
-              this.vehicleList[i].description = (!this.vehicleList[i].brand?"-": this.vehicleList[i].brand) + ' '
-                                              + (!this.vehicleList[i].model?"-": this.vehicleList[i].model) + ' '
-                                              + (!this.vehicleList[i].year?"-": this.vehicleList[i].year);
+          for (var i = 0; i < this.vehicleList.length; i++) {
+            this.vehicleList[i].description =
+              (!this.vehicleList[i].brand ? '-' : this.vehicleList[i].brand) +
+              ' ' +
+              (!this.vehicleList[i].model ? '-' : this.vehicleList[i].model) +
+              ' ' +
+              (!this.vehicleList[i].year ? '-' : this.vehicleList[i].year);
 
-              var per = this.personList.find(it => it._id == this.vehicleList[i].owner);
-
-              this.vehicleList[i].ownerName = !per?"":(per.names + " " + (!per.lastNames?"":per.lastNames) );
-            }
+              this.vehicleList[i].personName = this.vehicleList[i].owner.names + " "+ 
+              this.vehicleList[i].owner.lastNames
+          }
         }
       },
       err => {
-        if(err.substring(0,3)!= '404'){
+        if (err.substring(0, 3) != '404') {
           var msg = this.translate.instant('record_actions.error_occurred');
           this.toaster.error(err);
         }
@@ -167,43 +137,50 @@ export class FleetVehiclesComponent implements OnInit {
     this.currentState = 'ADD';
   }
 
-  edit(selected){
+  edit(selected) {
     this.selected = selected;
+
+    //depopulate
+
+    this.selected.company = selected.company._id;
+    this.selected.owner = selected.owner._id;
+
+    
     this.opened = true;
     this.currentState = 'EDIT';
   }
 
-  delete(selected){
+  delete(selected) {
     this.selected = selected;
 
-    this.vehicleService.deactivateData(selected._id).
-    toPromise()
-    .then( deleted => {
-      if(deleted){
-        this.toaster.success("Operación exitosa!");
-        this.getList();
-      }
-      else
-        {
-          this.toaster.error("Operación fallida!");
+    this.vehicleService
+      .deactivateData(selected._id)
+      .toPromise()
+      .then(deleted => {
+        if (deleted) {
+          this.toaster.success('Operación exitosa!');
+          this.getList();
+        } else {
+          this.toaster.error('Operación fallida!');
           return;
         }
-    }).
-    catch(err => {
-      this.toaster.error(err);
-      return;
-    })
-
-
+      })
+      .catch(err => {
+        this.toaster.error(err);
+        return;
+      });
   }
 
   confirmDelete(selected) {
-
     const confirmDialog = this.confirmDialog.open(ConfirmDialogComponent, {
       data: {
         title: this.translate.instant('record_actions.deactivate'),
-      //ODEM Cambiar la propiedad de select
-        message: this.translate.instant('notifications.can_deactivate') + ': ' + selected.description + ' ?',
+        //ODEM Cambiar la propiedad de select
+        message:
+          this.translate.instant('notifications.can_deactivate') +
+          ': ' +
+          selected.description +
+          ' ?',
         button1Text: this.translate.instant('buttons.yes').toUpperCase(),
         button2Text: this.translate.instant('buttons.no').toUpperCase(),
       },
@@ -214,11 +191,8 @@ export class FleetVehiclesComponent implements OnInit {
     });
   }
 
-  changeState(state: string){
+  changeState(state: string) {
     this.currentState = state;
-    if(state=='RETRIEVE')
-      this.getList();
+    if (state == 'RETRIEVE') this.getList();
   }
-
-
 }
